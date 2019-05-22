6a5feccc13c42ce03adb8691dd97be17264a2f8e
Miguel Acevedo <acevedomiguel@outlook.com>
2019-05-22 14:23:12 -0700

    Added added visibility option to portfolios & images

--- frontend/src/containers/PortfolioForm/PortfolioForm.js
+++ frontend/src/containers/PortfolioForm/PortfolioForm.js
@@ -1,24 +1,28 @@
 import React, { Component } from 'react';
 import axios from "axios";
-import { Button, FormGroup, FormControl, FormLabel } from "react-bootstrap";
+import { Button, FormGroup, FormControl, FormLabel, FormCheck } from "react-bootstrap";
 import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle } from 'reactstrap';
 import './PortfolioForm.css';
+// https://preview.themeforest.net/item/skylith-viral-creative-multipurpose-html-template/full_screen_preview/21214857?ref=cirvitis
+
+// TODO:
+// Add a "Add Image" button which adds an image to the portfolio images array.
+// Add a newImages to the state and add the new images there, then pass them through to a request.
+// Add a isVisible checkbox to the form.
 
 export default class PortfolioForm extends Component {
     constructor(props) {
         super(props);
 
-        console.log(props.data.images);
-        const hi = [];
-        const hiob = {
-            "url": "testing",
-            "fuck": "me",
-        };
-        hi.push(hiob);
+        console.log(props.data);
+
         this.state = {
-            portfolios: null,
-            url: null,
+            title: props.data.title,
+            about: props.data.about,
+            visible: props.data.visible,
+            id: props.data.id,
             images: props.data.images,
+            newImages: [], // Used to add new images.
         }
     }
 
@@ -26,12 +30,34 @@ export default class PortfolioForm extends Component {
         this.setState( {portfolios: "Test"} );
     }
 
-    handleChange = (event, index) => {
-        console.log(event.target.value);
+    handleInfoChange = event => {
+
+        const inputVal = event.target.value;
+        console.log("Testing");
+
+        this.setState({
+            [event.target.id]: event.target.value 
+        });
+
+      }
+
+    handleNewImageChange = (event, index) => {
 
         const inputVal = event.target.value;
         const inputId = event.target.id;
 
+        let newImages = [...this.state.newImages];
+        newImages[index][inputId] = inputVal;
+        this.setState({ newImages });
+
+    }
+
+    handleImageChange = (event, index) => {
+        console.log(event.target.value);
+
+        const inputVal = event.target.value;
+        const inputId = event.target.id;
+        /*
         this.setState(prevState => ({
             images: {
                 ...prevState.images,
@@ -40,29 +66,82 @@ export default class PortfolioForm extends Component {
                     [inputId] : inputVal
                 }
             },
-        }));
+        })); */
 
-      }
+        let images = [...this.state.images];
+        images[index][inputId] = inputVal;
+        images[index].update = true;
+        this.setState({ images });
+    }
 
-      handleSave = () => {
-        console.log("I will save data!")
-      }
+    // Add a new index to the newImages array.
+    handleNew = () => {
+        const imageInfo = {
+            "url": "",
+            "caption": "",
+            visible: true,
+        };
+        this.setState(prevState => {
+            const state = prevState;
+            const len = Object.keys(state.newImages).length;
+            //state.newImages.splice(len, 0, imageInfo);
+            state.newImages.push(imageInfo);
+            //state.newImages[len] = imageInfo;
+            return state;
+        });
+        //console.log(this.state);
+    }
+
+    handleDelete = (event, index, type) => {
+        event.preventDefault();
+        if (index > -1) {
+            //let newImages = [...this.state.newImages];
+            //newImages.splice(index, 1);
+            //this.setState({ newImages });
+            const deleted = [...this.state[type]];
+            deleted.splice(index, 1);
+            this.setState({ [type]: deleted });
+        }
+    }
+
+    handleVisible = (event, index, type) => {
+        // handle unchecking visibility here.
+        const inputId = event.target.id;
+        const checked = event.target.checked;
+
+        let updateState = [...this.state[type]];
+        updateState[index].visible = checked;
+        this.setState({ [type]: updateState });
+    }
+
+    handleSave = () => {
+        console.log(this.state);
+    }
 
     render() {
         const data = this.props.data;
         console.log(this.state);
 
-        const imageGroups = Object.keys(data.images).map(key => 
-            <CreateFormImageGroups key={key} data={data.images[key]} state={this.state} handleChange={this.handleChange} index={key}/>
+        const imageGroups = Object.keys(this.state.images).map(key => 
+            <CreateFormImageGroups key={key} data={this.state.images[key]} state={this.state} 
+            handleImageChange={this.handleImageChange} index={key} handleDelete={this.handleDelete} handleVisible={this.handleVisible} />
+        )
+        // Map over the new images and add their forms to the DOM.
+
+        const newImageGroups = Object.keys(this.state.newImages).map(key => 
+            <CreateNewFormImage key={key} data={this.state.newImages[key]} state={this.state} handleNewImageChange={this.handleNewImageChange} 
+            index={key} handleDelete={this.handleDelete} handleVisible={this.handleVisible} />
         )
 
         return (
             <div>
                 <Card>
                 <CardBody>
-                    <h3>{data.title}</h3>
                     <form>
+                        <CreateFormPortfolioInfo data={this.state} handleInfoChange={this.handleInfoChange} />
                         {imageGroups}
+                        {newImageGroups}
+                        <Button block onClick={this.handleNew}>Add New</Button>
                         <Button block onClick={this.handleSave}>Save</Button>
                     </form>
                 </CardBody>
@@ -72,9 +151,71 @@ export default class PortfolioForm extends Component {
     }
 }
 
+const CreateFormPortfolioInfo = (props) => {
+    const data = props.data;
+    return (
+        <div>
+            <FormGroup controlId="title" bssize="small">
+                <FormLabel>Title</FormLabel>
+                <FormControl
+                type="text"
+                value={data.title}
+                onChange={props.handleInfoChange}
+                />
+            </FormGroup>
+            <FormGroup controlId="about" bssize="small">
+                <FormLabel>About</FormLabel>
+                <FormControl
+                type="text"
+                value={data.about}
+                onChange={props.handleInfoChange}
+                />
+            </FormGroup>
+        </div>
+    );
+}
+
+const CreateNewFormImage = (props) => {
+    const data = props.data;
+    //console.log(data);
+    // Check if props.state.newImages is not null, then map through it and return newForm for each in it.
+    const type = "newImages";
+    const isVisible = props.state.newImages[props.index].visible;
+    return (
+        <Card className="image-form">
+            <CardBody className="image-form-body-new">
+                <FormGroup controlId="caption" bssize="small">
+                    <FormLabel>Image Caption</FormLabel>
+                    <FormControl
+                    type="text"
+                    value={props.state.newImages[props.index].caption}
+                    onChange={(e) => props.handleNewImageChange(e, props.index)}
+                    />
+                </FormGroup>
+                <FormGroup controlId="url" bssize="large">
+                    <FormLabel>Image URL</FormLabel>
+                    <FormControl
+                    type="text"
+                    value={props.state.newImages[props.index].url}
+                    onChange={(e) => props.handleNewImageChange(e, props.index)}
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <FormCheck onChange={(e) => props.handleVisible(e, props.index, type)} checked={isVisible} type="checkbox" label="Visible"/>
+                </FormGroup>
+                <Button variant="danger" onClick={(e) => props.handleDelete(e, props.index, type)} >Delete</Button>
+            </CardBody>
+        </Card>
+    );
+    // Return the new image form here.
+}
+
 const CreateFormImageGroups = (props) => {
     const data = props.data;
     console.log(data);
+    // Check if props.state.newImages is not null, then map through it and return newForm for each in it.
+    const type = "images";
+    const isVisible = props.state.images[props.index].visible;
     return (
         <Card className="image-form">
             <CardBody className="image-form-body">
@@ -83,7 +224,7 @@ const CreateFormImageGroups = (props) => {
                     <FormControl
                     type="text"
                     value={props.state.images[props.index].caption}
-                    onChange={(e) => props.handleChange(e, props.index)}
+                    onChange={(e) => props.handleImageChange(e, props.index)}
                     />
                 </FormGroup>
                 <FormGroup controlId="url" bssize="large">
@@ -91,9 +232,13 @@ const CreateFormImageGroups = (props) => {
                     <FormControl
                     type="text"
                     value={props.state.images[props.index].url}
-                    onChange={(e) => props.handleChange(e, props.index)}
+                    onChange={(e) => props.handleImageChange(e, props.index)}
                     />
                 </FormGroup>
+                <FormGroup>
+                    <FormCheck onChange={(e) => props.handleVisible(e, props.index, type)} checked={isVisible} type="checkbox" label="Visible"/>
+                </FormGroup>
+                <Button variant="danger" onClick={(e) => props.handleDelete(e, props.index, type)} >Delete</Button>
             </CardBody>
         </Card>
     );
